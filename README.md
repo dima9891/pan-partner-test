49506590845b1300bd4e6683fa6e7e129948d436
Дмитрий Резвых <rezvykh@mail.ru>
2023-01-25 17:10:28 +0400

    added packages and gulpfile for development

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pan-partner-test
+
+To check the site just open index.html in any browser
+
+To start develop:
+1) install packages - npm i
+2) after changing .scss files use gulp command to update .css
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+const concat = require('gulp-concat')
+
+function buildStyles() {
+    return gulp
+        .src("./sass/*.scss")
+        .pipe(concat('styles.css'))
+        .pipe(
+            sass.sync({ outputStyle: "compressed" }).on("error", sass.logError)
+        )
+        .pipe(gulp.dest("./css"))
+}
+
+exports.buildStyles = buildStyles
+
+exports.watch = function () {
+    gulp.watch("./sass/*.scss", ["sass"])
+}
+
+function defaultTask() {
+    return buildStyles()
+}
+
+exports.default = defaultTask
+
